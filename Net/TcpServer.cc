e21f087e04ca217b1ce915233976ffbefbf5bcdc
Lei114514 <lei235711@gmail.com>
2025-10-17 23:05:04 +0800

    實現單線程的TcpServer

---
+++ Net/TcpServer.cc
@@ -0,0 +1,114 @@
+#include <memory>
+#include <string>
+#include <string.h>
+
+#include "TcpServer.h"
+#include "TcpConnection.h"
+#include "EventLoop.h"
+#include "Socket.h"
+#include "Channel.h"
+#include "Acceptor.h"
+#include "InetAddress.h"
+#include "Logger.h"
+
+static EventLoop* checkLoopNotNull(EventLoop* loop)
+{
+    if(loop==nullptr)
+    {
+        LOG_FATAL("loop is NULL");
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop* loop
+            ,const InetAddress &listenAddr
+            ,const std::string &name
+            ,Option option)
+    : loop_(checkLoopNotNull(loop))
+    , acceptor_(std::make_unique<Acceptor>(loop,listenAddr,(option==Option::kReusePort?1:0)))
+    , ipPort_(listenAddr.toIpPort())
+    , nextConnId_(0)
+    , name_(name)
+    , started_(false)
+{
+    acceptor_->setNewConnectionCallback([this](int sockfd,const InetAddress& peerAddr)->void{
+        newConnection(sockfd,peerAddr);
+    });
+}
+TcpServer::~TcpServer()
+{
+    for(auto &item:connections_)
+    {
+        TcpConnectionPtr conn{item.second};
+        item.second.reset();
+        conn->getLoop()->runInLoop([conn]()->void{
+            conn->connectDestroyed();
+        });
+    }
+}
+void TcpServer::start()
+{
+    if(started_.fetch_add(1)==0)
+    {
+        loop_->runInLoop([this]()->void{
+            acceptor_->listen();
+            threadInitCallback_(loop_);
+        });
+    }
+}
+
+void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
+{
+    EventLoop* loop = loop_;
+
+    char buf[64];
+    snprintf(buf,sizeof(buf),"-%s#%d",ipPort_.c_str(),nextConnId_);
+    ++nextConnId_;
+    std::string connName=name_+buf;
+
+    LOG_DEBUG("TcpServer:%s - new Connection [%s] from %s",name_.c_str(),connName.c_str(),peerAddr.toIpPort().c_str());
+
+    sockaddr_in addr;
+    ::memset(&addr,0,sizeof(addr));
+    socklen_t optlen=sizeof(addr);
+    if(::getsockname(sockfd,reinterpret_cast<sockaddr*>(&addr),&optlen)<0)
+    {
+        LOG_ERROR("getsockname error, name=%s",connName.c_str());
+    }
+    InetAddress localAddr(addr);
+
+    TcpConnectionPtr conn=std::make_shared<TcpConnection>(loop,
+                                                          connName,
+                                                          sockfd,
+                                                          localAddr,
+                                                          peerAddr);
+    connections_[connName]=conn;
+
+    conn->setConnectionCallback(connectionCallback_);
+    conn->setMessageCallback(messageCallback_);
+    conn->setWriteCompleteCallback(writeCompleteCallback_);
+    
+    conn->setCloseCallback([this](const TcpConnectionPtr& connection)->void{
+        removeConnection(connection);
+    });
+
+    loop->runInLoop([conn]()->void{
+        conn->connectEstablished();
+    });
+}
+void TcpServer::removeConnection(const TcpConnectionPtr& conn)
+{
+    loop_->runInLoop([this,conn]()->void{
+        removeConntionInLoop(conn);
+    });
+}
+void TcpServer::removeConntionInLoop(const TcpConnectionPtr& conn)
+{
+    LOG_DEBUG("TcpServer:%s - remove Connection [%s]",name_.c_str(),conn->name().c_str());
+    
+    connections_.erase(conn->name());
+    EventLoop* loop=conn->getLoop();
+    loop->queueInLoop([conn]()->void{
+        conn->connectDestroyed();
+    });
+}
---
+++ Net/TcpServer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <atomic>
+#include <functional>
+
+#include "Callbacks.h"
+
+class EventLoop;
+class TcpConnection;
+class Socket;
+class Channel;
+class Acceptor;  
+class InetAddress;
+
+class TcpServer
+{
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    enum class Option
+    {
+        kNoReusePort,
+        kReusePort
+    };
+
+    TcpServer(EventLoop* loop
+              ,const InetAddress &listenAddr
+              ,const std::string &name
+              ,Option option = Option::kNoReusePort);
+    ~TcpServer();
+
+    void setThreadInitCallback(const ThreadInitCallback& cb){ threadInitCallback_ = cb; }
+    void setConnectionCallback(const ConnectionCallback &cb) { connectionCallback_ = cb; }
+    void setMessageCallback(const MessageCallback &cb) { messageCallback_ = cb; }
+    void setWriteCompleteCallback(const WriteCompleteCallback &cb) { writeCompleteCallback_ = cb; }
+
+    void start();
+private:
+    using ConnectionMap=std::unordered_map<std::string,TcpConnectionPtr>;
+
+    void newConnection(int sockfd, const InetAddress& peerAddr);
+    void removeConnection(const TcpConnectionPtr& conn);
+    void removeConntionInLoop(const TcpConnectionPtr& conn);
+
+    EventLoop* loop_;
+    std::unique_ptr<Acceptor> acceptor_;
+    ConnectionMap connections_;
+    const std::string ipPort_;
+
+    int nextConnId_;
+
+    const std::string name_;
+    std::atomic_int started_;
+    
+    ThreadInitCallback threadInitCallback_;
+
+    ConnectionCallback connectionCallback_;
+    MessageCallback messageCallback_;
+    WriteCompleteCallback writeCompleteCallback_;
+};
